b6b9d54d386f66ad9ae18883ba0ad2e11d6c056b
LorenaBH <lorenabenitoherrero@gmail.com>
2019-04-13 20:38:17 +0200

    patata10

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const foot = document.querySelector('.phrase');
+
+function ten (aliment){
+    for (let i = 0; i < 10; i++) {
+        console.log('patata');
+      }
+}
+//ten('patata');
