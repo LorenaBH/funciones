729ca86b346598ab9fec17173b2e6c7786d67969
LorenaBH <lorenabenitoherrero@gmail.com>
2019-04-13 21:58:02 +0200

    repeat funtions

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 'use strict';
-
-
-
 function ten (word){
     for (let i = 0; i < 10; i++) {
        console.log (word);
       }
 }
-ten('patata');
-ten('Aguacate');
-ten('Pizza');
+ten('patata',10);
+ten('Aguacate',8);
+ten('Pizza',5);
