cc1c5aa23e081b38e9f06df45806b364e19ec4be
LorenaBH <lorenabenitoherrero@gmail.com>
2019-04-14 11:52:54 +0200

    making getrandomnumber

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 'use strict';
-function ten (word){
-    for (let i = 0; i < 10; i++) {
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  getRandomNumber(10);
+  const result = getRandomNumber(10);
+  console.log(result);
+
+function ten (word,cantidad){
+    for (let i = 0; i < cantidad; i++) {
        console.log (word);
       }
 }
