3fc7f1ae46a1c0065c14af18f314026493c5f818
LorenaBH <lorenabenitoherrero@gmail.com>
2019-04-14 19:51:38 +0200

    parte 5

--- js/main.js
+++ js/main.js
@@ -3,14 +3,40 @@
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
-  console.log(result);
-
-function ten (word,cantidad){
+function writeThis (word,cantidad){
     for (let i = 0; i < cantidad; i++) {
        console.log (word);
       }
+    }
+    writeThis('patata',getRandomNumber(10));
+    writeThis('Aguacate',getRandomNumber(10));
+    writeThis('Pizza',getRandomNumber(10));
+
+const myWordList =  [
+  {
+    text: 'Pencil',
+    total: 6
+  },
+  {
+    text: 'Thermo',
+    total: 2
+  },
+  {
+    text: 'TV',
+    total: 8
+  },
+  {
+    text: 'Phone',
+    total: 4
+  }
+];
+
+function writeMyArray(arraycualquiera){
+  for (const data of arraycualquiera){
+    for (let i =0; i<data.total; i++){
+      console.log(data.text);
+    }
+  }
 }
-ten('patata',getRandomNumber(10) );
-ten('Aguacate',getRandomNumber(10));
-ten('Pizza',getRandomNumber(10));
+writeMyArray(myWordList);
 
